c6e261457d184b486fec905a79197586ed8bd82a
r2d23e <117255103+r2d23e@users.noreply.github.com>
2023-04-18 09:48:48 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+// let doc = document//получение объекта body
+
+// let body = doc.body//обращение к body
+// let html = doc.documentElement//обращение к самому домену
+
+
+//  let id  =doc.getElementById('uydi')
+//  let classes= doc.getElementsByClassName('wrap4')[0]
+//  let  wrap2= doc.getElementsByName('roma')
+//  let get =  doc.getElementsByTagName('p')
+
+
+//  console.log(id);
+//  console.log(classes);
+//  console.log(wrap2);
+//  console.log(get);
+
+
+
+//  let url = "https://www.visitdubai.com/-/media/gathercontent/article/t/top-rides-at-img-worlds-of-adventure/media/top-rides-at-img-worlds-of-adventure-predator-5.jpg?rev=f1bb54a15add49a09c912eac851f4ff7&cx=0.56&cy=0.4&cw=397&ch=397"
+//  let img = doc.querySelector('img')
+
+// img.setAttribute('src', url)
+
+// img.alt = 'картинка кода'
+
+// console.log(img.getAttribute('src'));
